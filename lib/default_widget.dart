2aa0c5eb879727fdcaaa9489bd3fe513fcf6120f
ahmedmelsayed74 <73236520+ahmedmelsayed74@users.noreply.github.com>
2025-05-30 21:47:04 +0300

    Update default_widget.dart

--- lib/default_widget.dart
+++ lib/default_widget.dart
@@ -1,39 +1,42 @@
 import 'package:flutter/material.dart';
 
-Widget defaultEditText({
-  int lines = 1,
-  Function()? onTap,
-  TextEditingController? controller,
-  required String hintText,
-  TextInputType textInput = TextInputType.text,
-  IconData? suffixIcon,
-  bool obscureText = false
-}){
+Widget حقل_نص_افتراضي({
+  int عدد_الأسطر = 1,
+  Function()? عند_النقر,
+  TextEditingController? وحدة_التحكم,
+  required String نص_إرشادي,
+  TextInputType نوع_الإدخال = TextInputType.text,
+  IconData? رمز_نهاية,
+  bool نص_سري = false,
+}) {
   return Padding(
     padding: const EdgeInsets.all(20.0),
     child: TextFormField(
-      maxLines: lines,
-      validator: (value) {
-        if (value == null || value.isEmpty) {
-          return 'Fill The Empty Data';
+      maxLines: عدد_الأسطر,
+      validator: (القيمة) {
+        if (القيمة == null || القيمة.isEmpty) {
+          return 'يرجى ملء الحقل الفارغ';
         }
         return null;
       },
-      onTap: onTap,
-     controller: controller,
-      keyboardType: textInput,
-      obscureText: obscureText,
+      onTap: عند_النقر,
+      controller: وحدة_التحكم,
+      keyboardType: نوع_الإدخال,
+      obscureText: نص_سري,
       decoration: InputDecoration(
-        label: Text(hintText,style: TextStyle(color:Colors.white,),),
+        label: Text(
+          نص_إرشادي,
+          style: TextStyle(color: Colors.white),
+        ),
         border: OutlineInputBorder(
-          borderSide: BorderSide(color:Colors.black,width: 10,style: BorderStyle.solid),
+          borderSide: BorderSide(color: Colors.black, width: 10, style: BorderStyle.solid),
           borderRadius: BorderRadius.circular(15.0),
         ),
         suffixIcon: IconButton(
           onPressed: () {
-            controller?.clear();
+            وحدة_التحكم?.clear();
           },
-          icon: Icon(suffixIcon,color:Colors.white,),
+          icon: Icon(رمز_نهاية, color: Colors.white),
         ),
       ),
     ),
